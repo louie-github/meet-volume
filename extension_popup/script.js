c1d395c429fe01a242ff7223c7fcb01cc3f4851b
Louie Torres <louie.btowers@gmail.com>
2021-09-16 21:50:25 +0800

    Add "Remove popup" button to popup

--- extension_popup/script.js
+++ extension_popup/script.js
@@ -1,9 +1,18 @@
-const mainButton = document.getElementById("main-button");
+const addButton = document.getElementById("add-button");
+const removeButton = document.getElementById("remove-button");
 
-mainButton.addEventListener("click", () => {
+addButton.addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {
       message: "setup",
+    });
+  });
+});
+
+removeButton.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      message: "teardown",
     });
   });
 });
