76a2b63801d3ea819f91e2e9ab27b1d1a6ce9fe2
Louie Torres <louie.btowers@gmail.com>
2021-09-16 21:46:35 +0800

    Remove monkeypatching scripts

--- meet_popup/monkeypatch-content-script.js
+++
@@ -1,9 +0,0 @@
-// Thanks: https://stackoverflow.com/a/9517879/8646315
-// "Use a content script to access the page context variables and functions"
-
-let s = document.createElement("script");
-s.src = chrome.runtime.getURL("meet_popup/monkeypatch.js");
-s.onload = function () {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
